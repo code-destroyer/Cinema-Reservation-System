060128f41f5792892b3f5db5c6fc39bd56e88097
Ivelin <benitezzz@abv.bg>
2015-05-11 15:08:33 +0300

    Minor fixes

--- magic_reservation_system.py
+++ magic_reservation_system.py
@@ -18,22 +18,22 @@ class CommandLineInterface:
         name = input('Step 1 (User): Choose name: ')
         if name == '':
             print('Invalid name!')
-            self.name_input()
+            return self.name_input()
         for i in '0123456789':
             if i in name:
                 print('Invalid name!')
-                self.name_input()
+                return self.name_input()
         return name
 
     def ticket_input(self):
         tickets = input('Step 1 (User) Choose tickets: ')
         if tickets == '':
             print('Invalid tickets input!')
-            self.ticket_input()
+            return self.ticket_input()
         for i in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
             if i in tickets:
                 print('Invalid tickets input')
-                self.ticket_input()
+                return self.ticket_input()
         return tickets
 
     def give_up_reservation(self):
@@ -48,37 +48,75 @@ class CommandLineInterface:
             self.give_up_reservation()
             return True
 
+    def movie_prompt_1(self):
+        movie_id = input("Enter movie_id: ")
+        if movie_id == '':
+            print('Invalid movie ID!')
+            return self.movie_prompt_1()
+        for i in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            if i in movie_id:
+                print('Invalid movie ID')
+                return self.movie_prompt_1()
+            else:
+                return movie_id
+
+    def movie_prompt_2(self):
+        movie_id = input('Step 2 (Movie): Choose a movie: ')
+        if movie_id == '':
+            print('Invalid movie ID!')
+            return self.movie_prompt_2()
+        for i in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            if i in movie_id:
+                print('Invalid movie ID')
+                return self.movie_prompt_2()
+        return movie_id
+
     def projection_availability_checker(self, tickets, movie_id):
         projection_id = input('Step 3 (Projection): Choose a projection: ')
+        if projection_id == '':
+            print('Invalid projection ID!')
+            return self.projection_availability_checker(tickets, movie_id)
+        for i in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            if i in projection_id:
+                print('Invalid projection ID')
+                return self.projection_availability_checker(tickets, movie_id)
+
         if int(projection_id) not in self.manager.projection_id_check(movie_id):
             print('No such projection for selected movie!')
-            self.projection_availability_checker(tickets, movie_id)
+            return self.projection_availability_checker(tickets, movie_id)
         else:
             if self.manager.taken_seats_check(projection_id) < int(tickets):
                 print("Not enough available seats!")
                 self.manager.available_seats = 0
-                self.projection_availability_checker(tickets, movie_id)
+                return self.projection_availability_checker(tickets, movie_id)
             else:
                 self.manager.store_taken_seats(projection_id)
                 for seat in self.manager.hall.taken_seats:
                     self.manager.hall.take_seats(seat)
+                return projection_id
         return projection_id
 
     def multiple_seat_selection(self, tickets):
         for i in range(1, int(tickets) + 1):
             seats = input('Step 4 (Seats): Choose seat {}: '.format(i))
             valid_tuple = self.tuple_helper(seats)
+            count_user_input = 0
+            for i in valid_tuple:
+                count_user_input += 1
+            if count_user_input > 2 or count_user_input < 2:
+                print('Invalid input!')
+                return self.multiple_seat_selection(tickets)
             if self.manager.hall.check_seat_validity(valid_tuple) and self.manager.hall.check_seat_availability(valid_tuple):
                 if valid_tuple in self.taken_by_you_seat:
                     print('You already booked that seat!')
                     self.taken_by_you_seat = []
-                    self.multiple_seat_selection(tickets)
+                    return self.multiple_seat_selection(tickets)
                 else:
                     self.taken_by_you_seat.append(valid_tuple)
             else:
                 print('Invalid or already taken seat!')
                 self.taken_by_you_seat = []
-                self.multiple_seat_selection(tickets)
+                return self.multiple_seat_selection(tickets)
 
     def cancel_name_check(self):
         name = input('Enter the name whose reservation you want to delete: ')
@@ -108,7 +146,7 @@ class CommandLineInterface:
                 print('Current movies:')
                 self.manager.show_movies()
             if command == 'show_movie_projections':
-                movie_id = input("Enter movie_id: ")
+                movie_id = self.movie_prompt_1()
                 movie_date = input("Enter date(optional): ")
                 self.manager.get_movie_name(movie_id, movie_date)
                 print(self.manager.show_movies_projections(int(movie_id), movie_date))
@@ -125,13 +163,9 @@ class CommandLineInterface:
                     self.cinema_interface()
                 print('Here is the list of the movies about to hit the big screen')
                 self.manager.show_movies()
-                movie_id = input('Step 2 (Movie): Choose a movie: ')
+                movie_id = self.movie_prompt_2()
                 print(self.manager.show_movies_projections(int(movie_id)))
                 projection = self.projection_availability_checker(tickets, movie_id)
-                #if self.give_up_reservation() is True:
-                #   pass
-                #else:
-                #    self.reservation_process()
                 print('Available seats (marked with a dot): ')
                 print(self.manager.hall.get_cinema_hall())
                 if self.give_up_reservation():
@@ -158,7 +192,7 @@ def main():
     print("Command list:\n 1 - show_movies\n 2 - show_movie_projections\n 3 - make_reservation\n 4 - cancel_reservation\n 5 - help\n 6 - exit \n")
     CLI.cinema_interface()
     #print(CLI.projection_availability_checker('2', '2'))
-
+    #print(CLI.movie_prompt_2())
 
 
 
